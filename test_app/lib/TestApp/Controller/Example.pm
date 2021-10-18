37a476325a91e3997608875a70992c6fdb258487
NozomuTsuruta <tuno.0806@icloud.com>
2021-10-18 14:47:00 +0900

    postでレスポンスを返すように

--- test_app/lib/TestApp/Controller/Example.pm
+++ test_app/lib/TestApp/Controller/Example.pm
@@ -1,11 +1,23 @@
 package TestApp::Controller::Example;
 use Mojo::Base 'Mojolicious::Controller', -signatures;
+use Data::Dumper;
 
 # This action will render a template
 sub welcome ($self) {
 
-  # Render template "example/welcome.html.ep" with message
-  $self->render(msg => 'Welcome to the Mojolicious real-time web framework!');
+    # Render template "example/welcome.html.ep" with message
+    $self->render(
+        msg => 'Welcome to the Mojolicious real-time web framework!' );
+}
+
+sub index {
+    my $self = shift;
+
+    my $name  = $self->req->param("name");
+    my $email = $self->req->param("email");
+
+    # my $params_hash = $self->req->params->to_hash;
+    $self->render( json => { name => $name, email => $email } );
 }
 
 1;
