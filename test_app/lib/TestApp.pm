5e9a72248b9ecf512162acb904e5a2c58bb60e31
NozomuTsuruta <tuno.0806@icloud.com>
2021-11-01 14:40:51 +0900

    db接続、簡単な処理の実装

--- test_app/lib/TestApp.pm
+++ test_app/lib/TestApp.pm
@@ -3,6 +3,11 @@ use Mojo::Base 'Mojolicious', -signatures;
 
 # This method will run once at server start
 sub startup ($self) {
+    our $DB_NAME = "test";
+    our $DB_USER = "postgres";
+    our $DB_PASS = "";
+    our $DB_HOST = "127.0.0.1";
+    our $DB_PORT = "5432";
 
     # Load configuration from config file
     my $config = $self->plugin('NotYAMLConfig');
@@ -13,6 +18,16 @@ sub startup ($self) {
     # Router
     $self->plugin('SecureCORS');
     my $r = $self->routes->to( 'cors.origin' => '*' );
+    my $dbh =
+      DBI->connect( "dbi:Pg:dbname=$DB_NAME;host=$DB_HOST;port=$DB_PORT",
+        "$DB_USER", "$DB_PASS" )
+      or die "$!\n Error: failed to connect to DB.\n";
+    $dbh->do(
+        "CREATE TABLE IF NOT EXISTS todos (
+			id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+			text varchar(255) NOT NULL
+	        ) ENGINE=InnoDB;"
+    );
 
     # Normal route to controller
     $r->get('/read')->to('Example#read');
--- test_app/lib/TestApp/Controller/Example.pm
+++ test_app/lib/TestApp/Controller/Example.pm
@@ -2,25 +2,42 @@ package TestApp::Controller::Example;
 use Mojo::Base 'Mojolicious::Controller', -signatures;
 use Mojo::Headers;
 use Data::Dumper;
+use DBI;
 
-sub read ($self) {
+sub read {
     my $self = shift;
+    my $dbh =
+      DBI->connect( "dbi:Pg:dbname=$DB_NAME;host=$DB_HOST;port=$DB_PORT",
+        "$DB_USER", "$DB_PASS" )
+      or die "$!\n Error: failed to connect to DB.\n";
+    my $sth = $dbh->prepare("SELECT * FROM todos;");
+    $sth->execute();
 
-    my $id   = $self->req->param("id");
-    my $text = $self->req->param("text");
+    my $ary_ref = $sth->fetchrow_arrayref();
 
-    # my $params_hash = $self->req->params->to_hash;
-    $self->render( json => { id => $id, text => $text } );
+    # my ( $a, $b ) = @$ary_ref;
+    $sth->finish();
+    $dbh->disconnect();
+
+    $self->render( json => $ary_ref );
 }
 
 sub create {
     my $self = shift;
-
-    my $id   = $self->req->param("id");
     my $text = $self->req->param("text");
+    my $dbh =
+      DBI->connect( "dbi:Pg:dbname=$DB_NAME;host=$DB_HOST;port=$DB_PORT",
+        "$DB_USER", "$DB_PASS" )
+      or die "$!\n Error: failed to connect to DB.\n";
+    my $sth = $dbh->prepare("INSERT INTO todos(text) VALUES(?);");
+    $sth->execute( $text->{text} );
+    $sth = $dbh->prepare("SELECT * FROM todos;");
+    $sth->execute();
+    my $ary_ref = $sth->fetchrow_arrayref();
+    $sth->finish();
+    $dbh->disconnect();
 
-    # my $params_hash = $self->req->params->to_hash;
-    $self->render( json => { id => $id, text => $text } );
+    $self->render( json => $ary_ref );
 }
 
 sub update {
@@ -29,18 +46,39 @@ sub update {
     my $id   = $self->req->param("id");
     my $text = $self->req->param("text");
 
-    # my $params_hash = $self->req->params->to_hash;
-    $self->render( json => { id => $id, text => $text } );
+    my $dbh =
+      DBI->connect( "dbi:Pg:dbname=$DB_NAME;host=$DB_HOST;port=$DB_PORT",
+        "$DB_USER", "$DB_PASS" )
+      or die "$!\n Error: failed to connect to DB.\n";
+    my $sth = $dbh->prepare("UPDATE todoes SET text=? WHERE id=?;");
+    $sth->execute( $text->{text}, $id->{id} );
+    $sth = $dbh->prepare("SELECT * FROM todos;");
+    $sth->execute();
+    my $ary_ref = $sth->fetchrow_arrayref();
+    $sth->finish();
+    $dbh->disconnect();
+
+    $self->render( json => $ary_ref );
 }
 
 sub delete {
     my $self = shift;
 
-    my $id   = $self->req->param("id");
-    my $text = $self->req->param("text");
+    my $id = $self->req->param("id");
 
-    # my $params_hash = $self->req->params->to_hash;
-    $self->render( json => { id => $id, text => $text } );
+    my $dbh =
+      DBI->connect( "dbi:Pg:dbname=$DB_NAME;host=$DB_HOST;port=$DB_PORT",
+        "$DB_USER", "$DB_PASS" )
+      or die "$!\n Error: failed to connect to DB.\n";
+    my $sth = $dbh->prepare("DELETE FROM todos WHERE id=?;");
+    $sth->execute( $id->{id} );
+    $sth = $dbh->prepare("SELECT * FROM todos;");
+    $sth->execute();
+    my $ary_ref = $sth->fetchrow_arrayref();
+    $sth->finish();
+    $dbh->disconnect();
+
+    $self->render( json => $ary_ref );
 }
 
 1;
